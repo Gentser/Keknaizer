438afe4da78221fd9bba79cfd47843a5d9ca8187
Максим Рымарь <max_rymar@mail.ru>
2019-06-01 23:07:09 +0300

    Update manipulator.cpp

--- manipulator.cpp
+++ manipulator.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "manipulator.h"
 
 #include <qfile.h>
@@ -41,45 +40,3 @@ void Manipulator::write(QJsonObject json) {
 //    save_file.write(json_string.toLocal8Bit());
     save_file.close();
 }
-=======
-#include "manipulator.h"
-
-#include <qfile.h>
-#include <qjsondocument.h>
-#include <qjsonobject.h>
-
-Manipulator::Manipulator() {
-    #if (defined (_WIN32) || defined (_WIN64))
-        pathToFile = "../OurProj/gantt"; //resolve this
-    #endif
-}
-
-QJsonObject Manipulator::read() {
-
-    QFile file(pathToFile);
-    if (!file.open(QIODevice::ReadOnly)) {
-        throw FileException(pathToFile.toStdString()); // some file exception?
-    }
-    QByteArray rawData = QByteArray::fromBase64(file.readAll());
-//    QByteArray rawData = file.readAll();
-    file.close();
-    QJsonDocument doc(QJsonDocument::fromJson(rawData));
-    return doc.object();
-}
-
-void Manipulator::write(QJsonObject json) {
-    QJsonDocument json_doc(json);
-    QString json_string = json_doc.toJson();
-
-    QFile save_file(pathToFile);
-    if (save_file.exists())
-        save_file.remove();
-    if (!save_file.open(QIODevice::WriteOnly)) {
-        throw FileException(pathToFile.toStdString()); //some file exception ?
-    }
-
-    save_file.write(json_string.toLocal8Bit().toBase64());
-//    save_file.write(json_string.toLocal8Bit());
-    save_file.close();
-}
->>>>>>> 70376e58ffc9b3f7820f5e9b2bcc0cad92d33f44
