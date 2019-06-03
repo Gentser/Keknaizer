f851f339d3cc94a61d05d63ad74209a43b69a43f
Максим Рымарь <max_rymar@mail.ru>
2019-06-03 18:26:39 +0300

    Add methods for timelin to edit Items(26+32+27)
    Добавлены методы в Timeline, чтобы можно было редактировать и удалять Item-ы
    из него, измененм конструктор в Timeline, добавлены новые исключения.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,75 +20,65 @@ MainWindow::MainWindow(QWidget *parent) :
             new GanttChart<std::string>();
 
     //Timeline
-    Timeline<std::string> *firstTimeline = new Timeline<std::string>("First_long_Timeline");
+    Timeline<std::string> *firstTimeline = new Timeline<std::string>("First_long_Timeline",
+                                                                     QDateTime(QDate(2005,1,1), QTime(0,0)),
+                                                                     QDateTime(QDate(2020,2,3), QTime(15,35)) );
 
 
     try{
         TimelineItem <std::string> *item = new TimelineItem<std::string>(QDateTime(QDate(2010,1,1), QTime(0,0)),
-                                                   QDateTime(QDate(2010,2,3), QTime(15,35)),
-                                                   std::string("Задача"));
+                                                       QDateTime(QDate(2010,2,3), QTime(15,35)),
+                                                       std::string("Задача"));
 
-        //Здесь exception
-//        TimelineItem <std::string> *newItem = new TimelineItem<std::string>(QDateTime(QDate(2010,1,1), QTime(0,0)),
-//                                                                        QDateTime(QDate(2009,2,3), QTime(15,35)),
-//                                                                        std::string("Задача"));
+            //Здесь exception
+    //        TimelineItem <std::string> *newItem = new TimelineItem<std::string>(QDateTime(QDate(2010,1,1), QTime(0,0)),
+    //                                                                        QDateTime(QDate(2009,2,3), QTime(15,35)),
+    //                                                                        std::string("Задача"));
 
-        //Item
-    firstTimeline->addItem(item);
-
-
+            //Item
+        firstTimeline->addItem(item);
 
 
 
-    firstTimeline->addItem(new TimelineItem<std::string>(QDateTime(QDate(2009,5,13), QTime(13,45)),
-                                                    QDateTime(QDate(2009,5,13), QTime(20,00)),
-                                                    std::string("А это вторая задача!")
-                              ));
-
-    firstTimeline->addItem(new TimelineItem<std::string>(QDateTime(QDate(2008,2,29), QTime(17,59)),
-                                                         QDateTime(QDate(2008,10,31), QTime(2,20)),
-                                                         std::string("А это третья задача!")
-                                   ));
 
 
-    Diagram->addTimeline(firstTimeline);
+        firstTimeline->addItem(new TimelineItem<std::string>(QDateTime(QDate(2009,5,13), QTime(13,45)),
+                                                        QDateTime(QDate(2009,5,13), QTime(20,00)),
+                                                        std::string("А это вторая задача!")
+                                  ));
 
-    qDebug() << "Name of firstTimeline " << QString::fromStdString(firstTimeline->getName());
-
-//    //Только для тетсирования
-//    for (int i = 0; i < Diagram->getTimelines()->at(0).getIntervals()->size(); i++){
-//        Diagram->getTimelines()->at(0).getIntervals()->at(i).print();
-//    }
-
-    //Iterator (ИСПОЛЬЗУЙТЕ ТАКОЙ ОБХОД ПО TimelineItem - ам)
-    for (auto iter = Diagram->getTimelines()->at(0).getIntervals()->begin();
-         iter != Diagram->getTimelines()->at(0).getIntervals()->end(); ++iter){
-        qDebug() << "Итерация" ;
-        iter->print();
-    }
-
-    //ТУТ ПРОБЛЕМА С ИТЕРАТОРОМ(который написали)
-//    for (auto iter = Diagram->getTimelines().at(0).begin();
-//         iter != Diagram->getTimelines().at(0).end(); ++iter){
-//        iter->get_value.print();
-//    }
+        firstTimeline->addItem(new TimelineItem<std::string>(QDateTime(QDate(2008,2,20), QTime(17,59)),
+                                                             QDateTime(QDate(2008,10,31), QTime(2,20)),
+                                                             std::string("А это третья задача!")
+                                       ));
 
 
-    int i = 0;
-    for (auto iter = Diagram->getTimelines()->at(0).getIntervals()->begin();
-         iter != Diagram->getTimelines()->at(0).getIntervals()->end(); ++iter){
-        qDebug() << "Итерация " << i++ << "name= " << QString::fromStdString(iter->getContent());
-        iter->print();
-    }
+        Diagram->addTimeline(firstTimeline);
 
-    //Find ItemByName Test from Timeline
-    try{
-        qDebug() << "НАШЛАСЬ" ;
-        firstTimeline->findItemByName("Задача")->print();
-        qDebug() << "НАШЛАСЬ" ;
-    } catch (FindItemNameException e){
-        qCritical() << "CATCH EXCEPT" << QString::fromStdString(e.getMessage()) ;
-    }
+        qDebug() << "Name of firstTimeline " << QString::fromStdString(firstTimeline->getName());
+
+    //    //Iterator (ИСПОЛЬЗУЙТЕ ТАКОЙ ОБХОД ПО TimelineItem - ам)
+    //    for (auto iter = Diagram->getTimelines()->at(0).getIntervals()->begin();
+    //         iter != Diagram->getTimelines()->at(0).getIntervals()->end(); ++iter){
+    //        qDebug() << "Итерация" ;
+    //        iter->print();
+    //    }
+
+
+        qDebug() << "ДО РЕДАКТИРОВАНИЯ" ;
+        int i = 0;
+        for (auto iter = Diagram->getTimelines()->at(0).getIntervals()->begin();
+             iter != Diagram->getTimelines()->at(0).getIntervals()->end(); ++iter){
+            qDebug() << "Итерация " << i++ << "name= " << QString::fromStdString(iter->getContent());
+            iter->print();
+        }
+
+        try{
+
+
+        } catch (Exception e){
+            qCritical() << "CATCH EXCEPT" << QString::fromStdString(e.getMessage()) ;
+        }
 
     } catch (StartGreaterThanEndException e){
         qCritical() << "CATCH EXCEPT" << QString::fromStdString(e.getMessage()) ;
