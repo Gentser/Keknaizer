dbb43b17b7cd06b6bc9a44edd72eedb2b12a138e
Axiino <max_rymar@mail.ru>
2019-06-05 23:16:48 +0300

    Add slot clickCalendar

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -21,9 +21,15 @@ public:
 
     GanttChart<std::string> *Diagram;
 
+    Timeline<std::string> *curTimeline;
+
+
+
 private slots:
     void on_PushButton_addTask_clicked();
     void on_PushButton_editTask_clicked();
+
+    void on_calendarWidget_clicked(const QDate &date);
 
 private:
     Ui::MainWindow *ui;
